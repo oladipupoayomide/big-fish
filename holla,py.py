5d496a19f02116c7daaed1f5aff6d06699b9afa7
oladipupoayomide <42249213+oladipupoayomide@users.noreply.github.com>
2018-08-09 18:02:00 +0000

    Add files via upload

---
+++ holla,py.py
@@ -0,0 +1,100 @@
+Python 3.4.0 (v3.4.0:04f714765c13, Mar 16 2014, 19:24:06) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+enter the age7
+what is your name?aishat
+aishat is 7 now
+enter the present year2018
+aishat is 7 now in 2018
+what is a century100
+100
+>>> ================================ RESTART ================================
+>>> 
+enter the age5
+what is your name?sope
+sope is 5 now
+enter the present year2018
+sope is 5 now in 2018
+what is a century100
+100
+>>> ================================ RESTART ================================
+>>> 
+enter your age5
+what is your name?aishat
+aishat is 5 now
+enter the present year2018
+aishat is 5 now in 2018
+>>> ================================ RESTART ================================
+>>> 
+enter your age5
+what is your name?aishat
+aishat is 5 now
+enter the present year2018
+aishat is 5 now in 2018
+what is the value of a century100
+aishat willbe 100 inthenext 95
+>>> ================================ RESTART ================================
+>>> 
+enter your age4
+what is your name?aisha
+aisha is 4 now
+enter the present year2018
+aisha is 4 now in 2018
+what is the value of a century100
+aisha willbe 100 inthenext 96
+>>> ================================ RESTART ================================
+>>> 
+enter your age5
+what is your name?sope
+sope is 5 now
+enter the present year2018
+sope is 5 now in 2018
+what is the value of a century100
+sope willbe 100 inthenext 95 years
+>>> ================================ RESTART ================================
+>>> 
+enter your age7
+what is your name?jude
+jude is 7 now
+enter the present year2018
+jude is 7 now in 2018
+what is the value of a century100
+jude willbe 100 inthenext 93 years
+>>> ================================ RESTART ================================
+>>> 
+enter your age4
+what is your name?eni
+eni is 4 now
+enter the present year2018
+eni is 4 now in 2018
+what is the value of a century100
+eni will be 100 in the next 96 years
+>>> ================================ RESTART ================================
+>>> 
+enter your age12
+what is your name?yeesha
+yeesha is 12 now
+enter the present year2018
+yeesha is 12 now in 2018
+what is the value of a century100
+yeesha willbe 100 inthenext 88 years
+>>> ================================ RESTART ================================
+>>> 
+enter your age5
+what is your name?s
+s is 5 now
+enter the present year2018
+s is 5 now in 2018
+what is the value of a century100
+s willbe 100 inthenext 95 years
+>>> ================================ RESTART ================================
+>>> 
+enter your age5
+what is your name?q
+q is 5 now
+enter the present year2018
+q is 5 now in 2018
+what is the value of a century100
+q will be 100 in the next 95 years
+>>> 
---
+++ sup.py
@@ -0,0 +1,16 @@
+#ask for integer
+age=int(input("enter your age"))
+name=(input("what is your name?"))
+print(name,"is",age,"now")
+year=int(input("enter the present year"))
+print(name,"is",age,"now","in",year)
+century =int(input("what is the value of a century"))
+print(name, "will",   "be",  100, "in",  "the",  "next" ,century-age, "years")
+
+
+a=3
+b=4
+c=5
+x=(-b+(4*a*c))/2*a
+print(x)
+
